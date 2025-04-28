cc396e23f3dafe44e089e96e11c96a82acac0f2d
Rahulsubramanian06 <92245492+Rahulsubramanian06@users.noreply.github.com>
2025-04-28 10:02:45 +0530

    useState base completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,47 @@
-import './App.css'
-import axios from 'axios'
-import { useState } from 'react'
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
-function  App(){
-  async function ap(){
-    try{
-      let reso = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
-      let secnd = reso.data
-      console.log(secnd)
-    }
-    catch(error){
+function App() {
+  async function ap() {
+    try {
+      let reso = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
+      let secnd = reso.data;
+      console.log(secnd);
+    } catch (error) {
       console.log(error);
     }
-  };
-  
-  ap()
-  let [btn, setBtn] = useState(0);
+  }
 
+  ap();
+  let [btn, setBtn] = useState(0);
+  let [boat, setBoat] = useState("");
   const addbtn = () => {
     setBtn((val) => {
-      return val +1
-    })
-  }
-const decbtn = () => {
-  setBtn((cval) => {
-    return cval -1
-  })
-}
+      return val + 1;
+    });
+  };
+  const decbtn = () => {
+    setBtn((cval) => {
+      return cval - 1;
+    });
+  };
+
+  const chval = (ch: React.ChangeEvent<HTMLInputElement>) => {
+    setBoat(ch.target.value);
+  };
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <h1>useState</h1>
       <h1>{btn}</h1>
+      <input type="text" onChange={chval} />
+      <h2>{boat}</h2>
       <button onClick={addbtn}>+1</button>
       <button onClick={decbtn}>-1</button>
-      {/* Your component content goes here */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
